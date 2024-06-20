6eaa0d0d7ce64b95fa24dc0e3aa60df7188935c4
jinalviranii <jinalvirani@gofynd.com>
2024-06-20 11:25:50 +0530

    Update package.json

--- src/router/guard.js
+++ src/router/guard.js
@@ -1,9 +1,13 @@
 import { setCompany } from '../helper/utils';
+import { setCompany, setApplication } from '../helper/utils';
 
 export const routeGuard = (to, from, next) => {
   const { params } = to;
   if (params.company_id) {
     setCompany(params.company_id);
+  }
+  if (params.application_id) {
+    setApplication(params.application_id);
   }
   next();
 };
